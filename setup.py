3983737afb4f5fa642e22e33a56c0c2e5ddb2acd
Russell Keith-Magee <russell@keith-magee.com>
2016-09-21 11:38:01 +0800

    Updated setup.py to enforce Python 3 and fix naming.

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@
 import io
 import re
 from setuptools import setup, find_packages
+import sys
 
 
-with io.open('./toga_cocoa/__init__.py', encoding='utf8') as version_file:
+if sys.version_info[:3] < (3, 4):
+    raise SystemExit("Toga requires Python 3.4+.")
+
+
+with io.open('./toga_django/__init__.py', encoding='utf8') as version_file:
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
     if version_match:
         version = version_match.group(1)
@@ -17,16 +22,15 @@ with io.open('README.rst', encoding='utf8') as readme:
 
 
 setup(
-    name='toga-cocoa',
+    name='toga-django',
     version=version,
-    description='A Cocoa (OS X) backend for the Toga widget toolkit.',
+    description='A Django backend for the Toga widget toolkit.',
     long_description=long_description,
     author='Russell Keith-Magee',
     author_email='russell@keith-magee.com',
     url='http://pybee.org/toga',
     packages=find_packages(exclude=['tests']),
     install_requires=[
-        'rubicon-objc',
     ],
     license='New BSD',
     classifiers=[
@@ -34,13 +38,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Environment :: MacOS X :: Cocoa',
-        'Operating System :: MacOS :: MacOS X',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Only',
         'Topic :: Software Development',
         'Topic :: Software Development :: User Interfaces',
         'Topic :: Software Development :: Widget Sets',
